8a6f63306f526437070321590319126412bdc77d
Div <divyangvashi@gmail.com>
2025-02-15 12:47:55 +0530

    no vs code errors

--- src/data/handRanges.ts
+++ src/data/handRanges.ts
@@ -11,5 +11,6 @@ export const handRanges: Record<Position, string[]> = {
         "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
     CO: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
         "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
+    BB: [],    
   };
   
