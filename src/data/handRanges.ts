5975fdc13cb56127a1842f9a83844cec4062faea
Div <divyangvashi@gmail.com>
2025-02-21 00:01:44 +0530

    real hand ranges

--- src/data/handRanges.ts
+++ src/data/handRanges.ts
@@ -1,16 +1,11 @@
 type Position = "BTN" | "SB" | "BB" | "UTG" | "MP" | "CO";
 
 export const handRanges: Record<Position, string[]> = {
-    BTN: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
-    "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
-    SB: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
-        "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
-    UTG: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
-        "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
-    MP: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
-        "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
-    CO: ["AA", "AKs", "AQs", "AJs", "ATs", "A9s", "A8s", "A7s", "A6s", "A5s", "A4s", "A3s", "A2s", "AKo", "KK", "KQs", "KJs", "KTs", "K9s", "AQo", "QQ", "QJs", "QTs",
-        "AJo", "JJ", "JTs", "ATo", "TT", "99", "88", "77", "66", "55", "44", "33", "22"],
+    BTN: ["AA",	"AKs",	"AQs",	"AJs",	"ATs",	"A9s",	"A8s",	"A7s",	"A6s",	"A5s",	"A4s",	"A3s",	"A2s","AKo",	"KK",	"KQs",	"KJs",	"KTs",	"K9s",	"K8s",	"K7s",	"K6s",	"K5s",	"K4s",	"K3s",	"K2s","AQo",	"KQo",	"QQ",	"QJs",	"QTs",	"Q9s",	"Q8s",	"Q7s",	"Q6s",	"Q5s",	"Q4s",	"Q3s",	"AJo",	"KJo",	"QJo",	"JJ",	"JTs",	"J9s",	"J8s",	"J7s",	"J6s",	"J5s",	"J4s",	"ATo",	"KTo",	"QTo",	"JTo",	"TT",	"T9s",	"T8s",	"T7s",	"T6s",	"A9o",	"K9o",	"Q9o",	"J9o",	"T9o",	"99",	"98s",	"97s",	"96s","A8o",	"K8o",	"T8o",	"98o",	"88",	"87s",	"86s",	"85s","A7o","77",	"76s",	"75s","A6o","66","65s",	"64s","A5o","55",	"54s",	"53s","A4o","44","33","22",],
+    SB: ["AA",	"AKs",	"AQs",	"AJs",	"ATs",	"A9s",	"A8s",	"A7s",	"A6s",	"A5s",	"A4s",	"A3s",	"A2s","AKo",	"KK",	"KQs",	"KJs",	"KTs",	"K9s",	"K8s",	"K7s",	"K6s",	"K5s",	"K4s",	"K3s",	"K2s","AQo",	"KQo",	"QQ",	"QJs",	"QTs",	"Q9s",	"Q8s",	"Q7s",	"Q6s",	"Q5s",	"Q4s",	"Q3s",	"Q2s","AJo",	"KJo",	"QJo",	"JJ",	"JTs",	"J9s",	"J8s",	"J7s",	"J6s",	"J5s",	"J4s",	"J3s",	"J2s","ATo",	"KTo",	"QTo",	"JTo",	"TT",	"T9s",	"T8s",	"T7s",	"T6s",	"T5s",	"T4s",	"T3s","A9o",	"K9o",	"Q9o",	"J9o",	"T9o",	"99",	"98s",	"97s",	"96s",	"95s",	"94s","A8o",	"K8o",	"Q8o",	"J8o",	"T8o",	"98o",	"88",	"87s",	"86s",	"85s",	"84s","A7o",	"K7o",	"Q7o",	"J7o",	"T7o",	"97o",	"87o",	"77",	"76s",	"75s",	"74s","A6o",	"K6o",	"Q6o","86o",	"76o",	"66",	"65s",	"64s",	"63s","A5o",	"K5o",	"Q5o","55",	"54s",	"53s","A4o",	"K4o",	"44",	"43s",	"A3o", "33","A2o","22"],
+    UTG: ["AA","AKs","AQs","AJs","ATs","A9s","A8s","A7s","A6s","A5s","A4s","A3s","AKo","KK","KQs","KJs","KTs","K9s","K8s","AQo","KQo","QQ","QJs","QTs","Q9s","AJo","KJo","QJo","JJ","JTs","J9s","ATo","TT","T9s","99","88","77","66"],
+    MP: ["AA",	"AKs",	"AQs",	"AJs",	"ATs",	"A9s",	"A8s",	"A7s",	"A6s",	"A5s",	"A4s",	"A3s",	"A2s", "AKo",	"KK",	"KQs",	"KJs",	"KTs",	"K9s",	"K8s",	"K7s",	"K6s", "AQo",	"KQo",	"QQ",	"QJs",	"QTs",	"Q9s",	"Q8s",	"AJo",	"KJo",	"QJo",	"JJo",	"JTs",	"J9s",	"ATo",	"KTo",	"QTo",	"TT",	"T9s",	"99",	"98s","88","87s","77",	"76s",	"66",	"55",	],
+    CO: ["AA",	"AKs",	"AQs",	"AJs",	"ATs",	"A9s",	"A8s",	"A7s",	"A6s",	"A5s",	"A4s",	"A3s",	"A2s", "AKo",	"KK",	"KQs",	"KJs",	"KTs",	"K9s",	"K8s",	"K7s",	"K6s",	"K5s",	"K4s",	"K3s", "AQo",	"KQo",	"QQ",	"QJs",	"QTs",	"Q9s",	"Q8s",	"Q7s",	"Q6s",	"AJo",	"KJo",	"QJo",	"JJ",	"JTs",	"J9s",	"J8s",	"ATo",	"KTo",	"QTo",	"JTo",	"TT",	"T9s",	"T8s",	"T7s",	"A9o",	"99",	"98s",	"97s",	"A8o",	"88",	"87s","77",	"76s","66","55","44","33"],
     BB: [],    
   };
   
